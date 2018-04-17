4598afe89aa1f2dc6be56ee01a25449b50f8a571
baglel <matti551@o2.pl>
2018-04-17 23:14:30 +0200

    r

--- WU/src/wu/OcenyController.java
+++ WU/src/wu/OcenyController.java
@@ -1,13 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wu;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +19,32 @@ import javafx.fxml.Initializable;
  */
 public class OcenyController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+     @FXML
+     private void assesment_menuActionButton(ActionEvent event) throws IOException {
+    
+        Parent assessment_page_parent = FXMLLoader.load(getClass().getResource("Admin_window.fxml"));
+        Scene assessment_page_scene = new Scene(assessment_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(assessment_page_scene);
+        app_stage.show();        
+     }
+     
+     @FXML
+    private void assesment_logoutActionButton(ActionEvent event) throws IOException {
+
+        Parent logout_page_parent = FXMLLoader.load(getClass().getResource("Login_window.fxml"));
+        Scene logout_page_scene = new Scene(logout_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(logout_page_scene);
+        app_stage.show();             
+    }
     
 }
--- WU/src/wu/Student_windowController.java
+++ WU/src/wu/Student_windowController.java
@@ -5,9 +5,17 @@
  */
 package wu;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -24,4 +32,41 @@ public class Student_windowController implements Initializable {
         // TODO
     }    
     
+    @FXML
+     private void assessment_studentButtonAction(ActionEvent event) throws IOException {
+    
+        Parent assessment_page_parent = FXMLLoader.load(getClass().getResource("Oceny.fxml"));
+        Scene assessment_page_scene = new Scene(assessment_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(assessment_page_scene);
+        app_stage.show();        
+     }
+     @FXML
+       private void close_studentButtonAction(ActionEvent event) throws IOException{
+        System.exit(0);  
+    }
+       
+       @FXML
+    private void users_studentButtonAction(ActionEvent event) throws IOException {
+
+        Parent logout_page_parent = FXMLLoader.load(getClass().getResource("usersCRUD_window.fxml"));
+        Scene logout_page_scene = new Scene(logout_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(logout_page_scene);
+        app_stage.show();                           
+    }
+    
+    
+    @FXML
+    private void logut_studentButtonAction(ActionEvent event) throws IOException {
+
+        Parent logout_page_parent = FXMLLoader.load(getClass().getResource("Login_window.fxml"));
+        Scene logout_page_scene = new Scene(logout_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(logout_page_scene);
+        app_stage.show();             
+    }
 }

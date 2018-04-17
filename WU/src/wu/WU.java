d78497777373bdf5b8afc1956045a68649cd8f25
baglel <matti551@o2.pl>
2018-04-17 21:20:33 +0200

    123

--- WU/src/wu/WU.java
+++ WU/src/wu/WU.java
@@ -7,7 +7,7 @@ package wu;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Parent;;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 

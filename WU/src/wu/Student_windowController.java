5e91af67bac12e63a70d7c46b91cd45791988081
Shauniak <zbigniew.stasiewicz@hotmail.com>
2018-04-17 20:35:17 +0200

    2.0

---
+++ WU/src/wu/Student_windowController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Zbyszek
+ */
+public class Student_windowController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

5ab9f6b0ef17df117608a6351315201ebb869688
JacekPol <j.polanski@psw.jaslo.pl>
2018-05-29 15:26:56 +0200

    Historia logowania Admin
    Historia logowania Admin

--- WU/src/wu/Loghistory_windowController.java
+++ WU/src/wu/Loghistory_windowController.java
@@ -1,8 +1,14 @@
 package wu;
 
+import Connection.ConnectionClass;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +17,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +28,13 @@ import javafx.stage.Stage;
  * @author Teo
  */
 public class Loghistory_windowController implements Initializable {
+
+    @FXML
+    private TableView<Loghistory> table_logHistory;
+    @FXML
+    private TableColumn<Loghistory, String> Columnlogin;
+    @FXML
+    private TableColumn<Loghistory, String> ColumnLastLogin;
 
     /**
      * Przyciski wystepujace w tej scenie
@@ -36,16 +52,54 @@ public class Loghistory_windowController implements Initializable {
     @FXML
     private Button close_history;
 
+    ConnectionClass PolaczenieDB = new ConnectionClass();
+
+    Connection sesja = PolaczenieDB.getConnection();
+    private ObservableList<Loghistory> data;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    } 
-    
+
+        data = FXCollections.observableArrayList();
+        Statement stmt = null;
+        Statement stmt2 = null;
+
+        try {
+
+            stmt = sesja.createStatement();
+            stmt2 = sesja.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT login_s as login,ostatnie_logowanie_studenta as ostatnie_logowanie FROM studenci;");
+
+            while (rs.next()) {
+                data.add(new Loghistory(rs.getString(1), rs.getString(2)));
+
+            }
+            
+            
+            
+            ResultSet rs2 = stmt2.executeQuery("SELECT login_p as login,ostatnie_logowanie_pracownicy as ostatnie_logowanie FROM pracownicy;");
+            while (rs2.next()) {
+                data.add(new Loghistory(rs2.getString(1), rs2.getString(2)));
+
+            }
+
+            Columnlogin.setCellValueFactory(new PropertyValueFactory<>("login"));
+            ColumnLastLogin.setCellValueFactory(new PropertyValueFactory<>("ostatnie_logowanie"));
+            table_logHistory.setItems(null);
+            table_logHistory.setItems(data);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
     /**
      * Metoda odpowiedzialna za przejscie do menu administratora
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void menuAdminButtonAction(ActionEvent event) throws IOException {
@@ -57,11 +111,13 @@ public class Loghistory_windowController implements Initializable {
         app_stage.setScene(assessment_page_scene);
         app_stage.show();
     }
-    
+
     /**
-     * Metoda odpowiedzialna za wylogowanie uzytkownika i przejscie do okna logowania
+     * Metoda odpowiedzialna za wylogowanie uzytkownika i przejscie do okna
+     * logowania
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void logoutAdminButtonAction(ActionEvent event) throws IOException {
@@ -73,48 +129,49 @@ public class Loghistory_windowController implements Initializable {
         app_stage.setScene(assessment_page_scene);
         app_stage.show();
     }
-    
+
     /**
      * Metoda odpowiedzialna za zamkniecie programu
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void closeAdminButtonAction(ActionEvent event) throws IOException {
         System.exit(0);
     }
-    
+
     /**
      * Metoda odpowiedzialna za czyszczenie zapytan
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void clearButtonAction(ActionEvent event) throws IOException {
-        
+
     }
-    
+
     /**
      * Metoda odpowiedzialna za zaladowanie danych po wcisnieciu przycisku
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void loadButtonAction(ActionEvent event) throws IOException {
-        
+
     }
-    
+
     /**
      * Metoda odpowiedzialna za wyszukanie uzytkownika po wcisnieciu przycisku
+     *
      * @param event
-     * @throws IOException 
+     * @throws IOException
      */
     @FXML
     private void searchButtonAction(ActionEvent event) throws IOException {
-        
+
     }
-    
-    
-    
-    
+
 }

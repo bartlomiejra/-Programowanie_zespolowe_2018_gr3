d799d1a7d14848d47ee63542a22a1201ef9aa8bd
JacekPol <j.polanski@psw.jaslo.pl>
2018-06-06 02:36:40 +0200

    admin historia logowania dodawanie

--- WU/src/wu/PrzedmiotAdmin_windowController.java
+++ WU/src/wu/PrzedmiotAdmin_windowController.java
@@ -1,8 +1,20 @@
 package wu;
 
+import Connection.ConnectionClass;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +23,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -23,7 +37,9 @@ import javafx.stage.Stage;
 public class PrzedmiotAdmin_windowController implements Initializable {
 
     @FXML
-    private TableView<?> table_przedmioty;
+    private TableView<Przedmiotyedycja> table_przedmioty;
+    @FXML
+    private TableColumn<Przedmiotyedycja, String> tf_NazwaPrzedmiotu_table;
     @FXML
     private TextField tf_NazwaPrzedmiotu;
     @FXML
@@ -45,16 +61,102 @@ public class PrzedmiotAdmin_windowController implements Initializable {
     @FXML
     private Button wyczyscPrzedmiot;
 
-    /**
-     * Initializes the controller class.
-     */
+    ConnectionClass PolaczenieDB = new ConnectionClass();
+
+    Connection sesja = PolaczenieDB.getConnection();
+    private ObservableList<Przedmiotyedycja> data;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    
+        
+
+
+        //wyswietlanie select przedmioty
+        data = FXCollections.observableArrayList();
+
+        Statement stmt = null;
+
+        try {
+
+            stmt = sesja.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT id_przedmiotu, nazwa_przedmiotu from przedmioty;");
+
+            //System.out.println("Dane:"+ rs.getString(2));
+            while (rs.next()) {
+
+                data.add(new Przedmiotyedycja(rs.getInt(1), rs.getString(2)));
+
+            }
+
+            //columnIdharmonogramuAdmin.setCellValueFactory(new PropertyValueFactory<>("idHarmonogramu"));
+          //  columnIdprzedmiotuPracownik.setCellValueFactory(new PropertyValueFactory<>("idPrzedmiotu"));
+            // columnIdpracownikaPracownik.setCellValueFactory(new PropertyValueFactory<>("idPracownika"));
+            tf_NazwaPrzedmiotu_table.setCellValueFactory(new PropertyValueFactory<>("nazwa_przedmiotu"));
+          
+
+            table_przedmioty.setItems(null);
+            table_przedmioty.setItems(data);
+
+        } catch (Exception e) {
+
+        }
+
+        // koniec wyswietlanie select przedmioty
+        
+        
+        
+          // filtrowanie tabeli student
+        
+        FilteredList<Przedmiotyedycja> filteredProdukt = new FilteredList<>(table_przedmioty.getItems(), e -> true);
+        tf_SzukajPrzedmioty.setOnKeyReleased(e -> {
+            tf_SzukajPrzedmioty.textProperty().addListener((observableValue, oldValue, newValue) -> {
+                filteredProdukt.setPredicate((Predicate<? super Przedmiotyedycja>) k -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String lcFilter = newValue.toLowerCase();
+                    if (k.getnazwa_przedmiotu().toLowerCase().contains(lcFilter)) {
+                        return true;
+                    }
+                    return false;
+                });
+            });
+            SortedList<Przedmiotyedycja> sortedProdukty = new SortedList<>(filteredProdukt);
+            table_przedmioty.setItems(sortedProdukty);
+        });
+        
+        // filtrowanie tabeli student koniec
     }    
 
     @FXML
     private void addPrzedmiotAdminButtonAction(ActionEvent event) {
+        
+          System.out.println("Dodawanie pracownika");
+        //System.out.println(data_ur_p.getEditor().getText());
+        
+          
+        
+        try {
+          
+            //Statement stmt = sesja.createStatement();
+            //System.out.println(data_ur_p.getEditor().getText());
+
+            sesja.createStatement().executeUpdate("INSERT INTO `przedmioty` (`id_przedmiotu`, `nazwa_przedmiotu`) VALUES (null,'" + tf_NazwaPrzedmiotu.getText() + "');");
+
+            Parent assessment_page_parent = FXMLLoader.load(getClass().getResource("przedmiotAdmin_window.fxml"));
+            Scene assessment_page_scene = new Scene(assessment_page_parent);
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            app_stage.hide();
+            app_stage.setScene(assessment_page_scene);
+            app_stage.show();
+            // cIPracownik.getSelectionModel().getSelectedItem().getid_pracownika()
+        } catch (Exception e) {
+
+        }
+        
     }
 
     @FXML
